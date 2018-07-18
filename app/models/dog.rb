5481c2bc16db7e1145bc4161cd3d35bc7a7cba6e
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-18 08:47:58 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,10 +2,13 @@ class Dog < ActiveRecord::Base
   attr_accessor :name, :breed, :age
 
   def initialize(name, breed, age)
-    @name = name 
-    @breed = breed 
-    @age = age 
-  end
+    @name = name
+    @breed = breed
+    @age = age
+    @@all << self
+  end 
+
+  def 
 
 
 end
